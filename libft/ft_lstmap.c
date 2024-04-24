f564ea12eceb48fa919c3ecc11eb21d5fcec0f65
Tiago Leite <tiagobscleite@gmail.com>
2024-04-24 19:02:45 +0100

    libft done

---
+++ libft/ft_lstmap.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tborges- <tborges-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/24 18:59:22 by tborges-          #+#    #+#             */
+/*   Updated: 2024/04/24 19:01:53 by tborges-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*new_lst;
+	t_list	*elem;
+
+	if (!lst)
+		return (0);
+	new_lst = 0;
+	while (lst)
+	{
+		if (!(elem = ft_lstnew(f(lst->content))))
+		{
+			ft_lstclear(&new_lst, del);
+			return (0);
+		}
+		ft_lstadd_back(&new_lst, elem);
+		lst = lst->next;
+	}
+	return (new_lst);
+}
