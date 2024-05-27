b84a7e27ef089fa5983a352529294712cba3af6d
Tiago Leite <147312116+leite-tiago@users.noreply.github.com>
2024-05-27 16:36:55 +0100

    42 printf

---
+++ ft_printf.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tborges- <tborges-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/20 17:12:44 by tborges-          #+#    #+#             */
+/*   Updated: 2024/05/27 15:24:43 by tborges-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+int	ft_printf(const char *format, ...);
+int	print_char(int c);
+int	print_str(char *str);
+int	print_ptr(unsigned long long ptr);
+int	print_nbr(int nbr);
+int	print_unsigned(unsigned int num);
+int	print_hex(unsigned long long num, const char specifier);
+int	print_percent(void);
+
+#endif
---
+++ prints1.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   prints1.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tborges- <tborges-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/25 20:44:05 by tborges-          #+#    #+#             */
+/*   Updated: 2024/05/27 15:31:47 by tborges-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	print_char(int c)
+{
+	return (write(1, &c, 1));
+}
+
+int	print_str(char *str)
+{
+	int	count;
+
+	if (!str)
+		return (write(1, "(null)", 6));
+	count = 0;
+	while (str[count])
+	{
+		count += print_char(str[count]);
+	}
+	return (count);
+}
+
+int	print_ptr(unsigned long long num)
+{
+	int	count;
+
+	if (!num)
+		return (print_str("(nil)"));
+	count = print_str("0x");
+	count += print_hex(num, 'x');
+	return (count);
+}
