cd144389021622e027d551010e7c1a187e0ce596
Tiago Leite <tiagobscleite@gmail.com>
2024-05-27 09:55:23 +0100

    print_nbr upgrade

--- printf/prints2.c
+++ printf/prints2.c
@@ -6,7 +6,7 @@
 /*   By: tborges- <tborges-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/25 20:45:25 by tborges-          #+#    #+#             */
-/*   Updated: 2024/05/26 19:10:27 by tborges-         ###   ########.fr       */
+/*   Updated: 2024/05/27 09:54:07 by tborges-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,9 +25,7 @@ int	print_nbr(int nbr)
 	}
 	else
 		n = (unsigned int)nbr;
-	if (n >= 10)
-		count += print_nbr(n / 10);
-	count += print_char(n % 10 + '0');
+	count += print_unsigned(n);
 	return (count);
 }
 
