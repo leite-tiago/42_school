767f9be70284ea76077c212b01e80be934f05d3e
Tiago Leite <tiagobscleite@gmail.com>
2024-11-16 22:25:44 +0000

    colors interface grafica

--- so_long/tests/colors.c
+++ so_long/tests/colors.c
@@ -1,3 +1,5 @@
+#include <mlx.h>
+#include <unistd.h>
 
 /**
  * create an TRGB int (0xTTRRGGBB)
@@ -76,10 +78,42 @@ int	get_opposite(int color)
 	return (create_trgb(t, r, g, b));
 }
 
-#include <stdio.h>
+typedef struct s_data
+{
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}			t_data;
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	/**
+		* The line_length differs from the actual window width.
+		* We therefore should ALWAYS calculate the memory offset
+		* using the line length set by mlx_get_data_addr.
+		*
+		* int offset = (y * line_length + x * (bits_per_pixel / 8));
+		*/
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int *)dst = color;
+}
 
 int	main(void)
 {
+	void	*mlx;
+	void	*mlx_win;
+	t_data	img;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Colors!");
+	img.img = mlx_new_image(mlx, 1920, 1080);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+			&img.endian);
+
 	int		color;
 	double	distance;
 	int		shaded_color;
@@ -89,9 +123,45 @@ int	main(void)
 	distance = 0.5;
 	shaded_color = add_shade(distance, color);
 	opposite = get_opposite(color);
-	printf("Cor original: #%06X\n", color);
-	printf("Cor sombreamento: #%06X\n", shaded_color);
-	printf("Cor original: #%06X\n", color);
-	printf("Cor invertida: #%06X\n", opposite);
-	return (0);
+
+	for (int y = 0; y <= 1080; y++)
+	{
+		for (int x = 0; x <= 1920; x++)
+			my_mlx_pixel_put(&img, x, y, color);
+	}
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	sleep(3);
+	for (int y = 0; y <= 1080; y++)
+	{
+		for (int x = 0; x <= 1920; x++)
+			my_mlx_pixel_put(&img, x, y, shaded_color);
+	}
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	sleep(3);
+	for (int y = 0; y <= 1080; y++)
+	{
+		for (int x = 0; x <= 1920; x++)
+			my_mlx_pixel_put(&img, x, y, opposite);
+	}
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	sleep(3);
+	mlx_loop(mlx);
 }
+
+// int	main(void)
+// {
+// 	int		color;
+// 	double	distance;
+// 	int		shaded_color;
+// 	int		opposite;
+
+// 	color = create_trgb(0, 18, 52, 86);
+// 	distance = 0.5;
+// 	shaded_color = add_shade(distance, color);
+// 	opposite = get_opposite(color);
+// 	printf("Cor original: #%06X\n", color);
+// 	printf("Cor sombreamento: #%06X\n", shaded_color);
+// 	printf("Cor original: #%06X\n", color);
+// 	printf("Cor invertida: #%06X\n", opposite);
+// 	return (0);
+// }
